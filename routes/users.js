8420e9d45c454998e02b354131244e1ae33eea6a
Irsyad <irsyart@gmail.com>
2018-03-02 18:14:35 +0700

    Added: static json output

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var users = [
+    {
+      id: 1,
+  	  username: "express"
+    },
+    {
+      id: 2,
+  	  username: "react"
+    }
+  ]
+  res.send(users);
 });
 
 module.exports = router;
